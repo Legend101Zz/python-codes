0a48de0fdfe1b04d99d18896ca6b4d8535bb2cfb
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2023-11-09 22:26:47 +0530

    changes in code

--- lists.py
+++ lists.py
@@ -1,8 +1,9 @@
 # write a python program to print duplicates in a list
 
 
+import random
 l = ["hello", 10, 20, 10, 20, 10, 30, 'm', 'm']
-
+m = ['1', '2', '3']
 # not allowed
 text = 'hello'
 # text[2]='l'
@@ -12,4 +13,6 @@ for ele in l:
     if l.count(ele) > 1 and ele not in d:
         d.append(ele)
 
-print(d, text[1:3])
+new_list = [x for (i, x) in enumerate(l) if i not in (0, 3, 5)]
+random.shuffle(m)
+print(d, text[1:3], new_list, m)
